b62b5e3c8daca9fa54cd5dcb18ecf67159b4a79f
firepenguindisopanda <nicosmith.smith3@gmail.com>
2024-10-01 00:21:17 -0400

    Added test for error handlers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,6 +139,11 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_account(self):
         """It should Update an existing Account"""
